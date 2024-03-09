e1261a85bf379bd14b9acceda7942a8e3d9c741b
Milos Paunovic <paun992@hotmail.com>
2024-03-09 15:36:58 +0100

    chore(ui): initial change of vscode to monaco editor

--- ui/src/components/namespace/Editor.vue
+++ ui/src/components/namespace/Editor.vue
@@ -1,211 +1,162 @@
 <template>
-    <top-nav-bar :title="routeInfo.title">
-        <template #additional-right>
-            <namespace-select
-                class="fit-content"
-                data-type="flow"
-                :value="namespace"
-                @update:model-value="namespaceUpdate"
-                allow-create
-                :is-filter="false"
-            />
-
-            <el-dropdown>
-                <el-button :icon="Plus" class="p-2 m-0" />
-                <template #dropdown>
-                    <el-dropdown-menu>
-                        <el-dropdown-item :icon="FilePlus" @click="pickFile">
-                            <input
-                                ref="filePicker"
-                                type="file"
-                                multiple
-                                style="display: none"
-                                @change="importNsFiles"
-                            >
-                            {{ $t("namespace files.import.file") }}
-                        </el-dropdown-item>
-                        <el-dropdown-item :icon="FolderPlus" @click="pickFolder">
-                            <input
-                                ref="folderPicker"
-                                type="file"
-                                webkitdirectory
-                                mozdirectory
-                                msdirectory
-                                odirectory
-                                directory
-                                style="display: none"
-                                @change="importNsFiles"
-                            >
-                            {{ $t("namespace files.import.folder") }}
-                        </el-dropdown-item>
-                    </el-dropdown-menu>
-                </template>
-            </el-dropdown>
-            <el-tooltip :hide-after="50" :content="$t('namespace files.export')">
-                <el-button :icon="FolderZip" class="p-2 m-0" @click="exportNsFiles" />
-            </el-tooltip>
-            <trigger-flow
-                ref="triggerFlow"
-                :disabled="!flow"
-                :flow-id="flow"
-                :namespace="namespace"
-            />
-        </template>
-    </top-nav-bar>
-    <iframe
-        style="visibility:hidden;"
-        onload="this.style.visibility = 'visible';"
-        v-if="namespace"
-        class="vscode-editor"
-        :src="vscodeIndexUrl"
-        ref="vscodeIde"
-    />
-    <section v-else class="container">
-        <el-alert type="info" :closable="false">
-            {{ $t("namespace choice") }}
-        </el-alert>
-    </section>
+    <div class="editor-wrapper">
+        <div class="file-explorer">
+            <h3>File Tree</h3>
+            <ul>
+                <li v-for="(file, index) in exampleFiles" :key="index" @click="loadFile(file)">
+                    {{ file.name }}
+                    <el-button type="danger" size="mini" @click.stop="deleteFile(index)">
+                        Delete
+                    </el-button>
+                </li>
+            </ul>
+            <button @click="addNewFile">
+                Add New File
+            </button>
+        </div>
+        <div class="editor-container">
+            <div v-for="(file, index) in openedFiles" :key="index" class="editor-tab">
+                <span @click="switchFile(index)">{{ file.name }}</span>
+                <el-button type="text" size="mini" @click.stop="closeFile(index)" class="close-button">
+                    X
+                </el-button>
+            </div>
+            <div ref="editorRef" class="editor" />
+        </div>
+    </div>
 </template>
 
-<script setup>
-    import NamespaceSelect from "./NamespaceSelect.vue";
-    import TopNavBar from "../layout/TopNavBar.vue";
-    import TriggerFlow from "../flows/TriggerFlow.vue";
-    import Plus from "vue-material-design-icons/Plus.vue";
-    import FolderPlus from "vue-material-design-icons/FolderPlus.vue";
-    import FilePlus from "vue-material-design-icons/FilePlus.vue";
-    import FolderZip from "vue-material-design-icons/FolderZip.vue";
-</script>
-
-<script>
-    import RouteContext from "../../mixins/routeContext";
-    import RestoreUrl from "../../mixins/restoreUrl";
-    import {apiUrl} from "override/utils/route";
-    import {mapState} from "vuex";
-    import {storageKeys} from "../../utils/constants";
+  <script>
+    import {ref, onMounted, onBeforeUnmount} from "vue";
+    import * as monaco from "monaco-editor";
 
     export default {
-        mixins: [RouteContext, RestoreUrl],
-        methods: {
-            importNsFiles(event) {
-                const files = [...event.target.files];
-                Promise.all(files.map(file => {
-                    return this.$store
-                        .dispatch("namespace/importFile", {
-                            namespace: this.namespace,
-                            file: file
-                        })
-                })).then(() => {
-                    this.$message({
-                        message: this.$t("namespace files.import.success"),
-                        type: "success"
-                    });
-                }).catch(() => {
-                    this.$message({
-                        message: this.$t("namespace files.import.error"),
-                        type: "error"
-                    });
-                }).finally(() => {
-                    this.$refs.vscodeIde.contentDocument.location.reload(true);
-                    event.target.value = "";
-                });
+        name: "MonacoEditor",
+        props: {
+            initialContent: {
+                type: String,
+                default: "",
             },
-            exportNsFiles() {
-                this.$store.dispatch("namespace/exportFiles", {
-                    namespace: this.namespace
-                });
-            },
-            pickFile() {
-                this.$refs.filePicker.click();
-            },
-            pickFolder() {
-                this.$refs.folderPicker.click();
-            },
-            namespaceUpdate(namespace) {
-                localStorage.setItem(storageKeys.LATEST_NAMESPACE, namespace);
-                this.$router.push({
-                    params: {
-                        namespace
-                    }
-                });
-            },
-            handleTabsDirty(tabs) {
-                // Add tabs not saved
-                this.tabsNotSaved = this.tabsNotSaved.concat(tabs.dirty)
-                // Removed tabs closed
-                this.tabsNotSaved = this.tabsNotSaved.filter(e => !tabs.closed.includes(e))
-                this.$store.dispatch("core/isUnsaved", this.tabsNotSaved.length > 0);
-            }
         },
-        data() {
-            return {
-                flow: null,
-                tabsNotSaved: [],
-                uploadFileName: undefined
-            }
-        },
-        mounted() {
-            window.addEventListener("message", (event) => {
-                const message = event.data;
-                if (message.type === "kestra.tabFileChanged") {
-                    const flowsFolderPath = `/${this.namespace}/_flows/`;
-                    const filePath = message.filePath.path;
-                    if (filePath.startsWith(flowsFolderPath)) {
-                        const fileName = filePath.split(flowsFolderPath)[1];
-                        // trim the eventual extension
-                        this.flow = fileName.split(".")[0];
-                    } else {
-                        this.flow = undefined;
-                    }
-                } else if (message.type === "kestra.tabsChanged") {
-                    this.handleTabsDirty(message.tabs);
-                } else if (message.type === "kestra.flowSaved") {
-                    this.$refs.triggerFlow.loadDefinition();
+        setup(props) {
+            const editorRef = ref(null);
+            let editorInstance = null;
+            const exampleFiles = ref([
+                {name: "File1.js", content: "function exampleFunction1() {\n  // Function 1 body\n}"},
+                {name: "File2.js", content: "function exampleFunction2() {\n  // Function 2 body\n}"},
+                {name: "File3.js", content: "function exampleFunction3() {\n  // Function 3 body\n}"},
+            ]);
+            const openedFiles = ref([]);
+
+            const loadFile = (file) => {
+                if (!openedFiles.value.some((openedFile) => openedFile.name === file.name)) {
+                    openedFiles.value.push(file);
+                    editorInstance.setValue(file.content);
+                }
+            };
+
+            const addNewFile = () => {
+                const newName = `NewFile${openedFiles.value.length + 1}.js`;
+                const newContent = "function newFunction() {\n  // New function body\n}";
+                exampleFiles.value.push({name: newName, content: newContent});
+                loadFile({name: newName, content: newContent});
+            };
+
+            const deleteFile = (index) => {
+                exampleFiles.value.splice(index, 1);
+                const openedFileIndex = openedFiles.value.findIndex((file) => file.name === exampleFiles.value[index].name);
+                if (openedFileIndex !== -1) {
+                    openedFiles.value.splice(openedFileIndex, 1);
+                }
+            };
+
+            const switchFile = (index) => {
+                const file = openedFiles.value[index];
+                editorInstance.setValue(file.content);
+            };
+
+            const closeFile = (index) => {
+                openedFiles.value.splice(index, 1);
+                if (index === openedFiles.value.length) {
+                    index--;
+                }
+                if (index >= 0) {
+                    switchFile(index);
+                } else {
+                    editorInstance.setValue("");
+                }
+            };
+
+            onMounted(() => {
+                editorInstance = monaco.editor.create(editorRef.value, {
+                    value: props.initialContent,
+                    language: "javascript",
+                });
+            });
+
+            onBeforeUnmount(() => {
+                if (editorInstance) {
+                    editorInstance.dispose();
                 }
             });
 
-            // Setup namespace
-            const namespace = localStorage.getItem(storageKeys.LATEST_NAMESPACE) ? localStorage.getItem(storageKeys.LATEST_NAMESPACE) : localStorage.getItem(storageKeys.DEFAULT_NAMESPACE);
-            if (namespace) {
-                this.namespaceUpdate(namespace);
-            } else if (this.namespaces?.length > 0) {
-                this.namespaceUpdate(this.namespaces[0]);
-            } else if (localStorage.getItem("tourDoneOrSkip") !== "true") {
-                this.$router.push({
-                    name: "flows/create",
-                    params: {
-                        tenant: this.$route.params.tenant
-                    }
-                });
-            }
+            return {editorRef, exampleFiles, openedFiles, loadFile, addNewFile, deleteFile, switchFile, closeFile};
         },
-        computed: {
-            ...mapState("namespace", ["namespaces"]),
-            routeInfo() {
-                return {
-                    title: this.$t("editor")
-                };
-            },
-            theme() {
-                return localStorage.getItem("theme") || "light";
-            },
-            vscodeIndexUrl() {
-                const uiSubpath = KESTRA_UI_PATH === "./" ? "/" : KESTRA_UI_PATH;
-                return `${uiSubpath}vscode.html?KESTRA_UI_PATH=${uiSubpath}&KESTRA_API_URL=${apiUrl(this.$store)}&THEME=${this.theme}&namespace=${this.namespace}`;
-            },
-            namespace() {
-                return this.$route.params.namespace;
-            }
-        }
-    }
-</script>
+    };
+  </script>
 
-<style lang="scss">
-    .fit-content {
-        width: fit-content;
-    }
-    .vscode-editor {
-        height: calc(100vh - 64.8px);
-        width: 100%;
-    }
-</style>
+  <style>
+  .editor-wrapper {
+    display: flex;
+  }
+
+  .file-explorer {
+    flex: 0 0 200px;
+    padding: 10px;
+  }
+
+  .file-explorer h3 {
+    margin-top: 0;
+  }
+
+  .file-explorer ul {
+    list-style: none;
+    padding: 0;
+  }
+
+  .file-explorer li {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    margin-bottom: 5px;
+  }
+
+  .file-explorer li button {
+    margin-left: auto;
+  }
+
+  .editor-container {
+    flex: 1;
+    padding: 10px;
+  }
+
+  .editor-tab {
+    display: inline-block;
+    padding: 5px 10px;
+    margin-right: 5px;
+    cursor: pointer;
+  }
+
+  .editor-tab span {
+    cursor: pointer;
+  }
+
+  .editor {
+    width: 100%;
+    height: 600px;
+  }
+
+  .close-button {
+    color: red;
+  }
+  </style>
